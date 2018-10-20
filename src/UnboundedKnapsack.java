9488666be045e9d7063fcc9cd63164f89e4f7af5
Zemlyakov Stas <redtom@yandex.ru>
2018-10-20 22:51:48 +0300

    Implement maximum value from other side

--- src/UnboundedKnapsack.java
+++ src/UnboundedKnapsack.java
@@ -1,5 +1,4 @@
 import static java.lang.Math.max;
-import static java.lang.Math.min;
 
 public class UnboundedKnapsack {
 
@@ -10,35 +9,35 @@ public class UnboundedKnapsack {
     for (int i = 0; i <= W; i++) {
       for (int j = 0; j < n; j++) {
         int currentBottleWeight = wt[j];
-        if (currentBottleWeight <= i) { // если вес бутылки меньше текущей итерации
+        if (currentBottleWeight <= i) {
           int iMinusWeight = i - currentBottleWeight;
           int currentValue = val[j];
           int newCompare = maximumValues[iMinusWeight] + currentValue;
           int currentMax = maximumValues[i];
-          maximumValues[i] = max(currentMax, newCompare); // максимльное значение между текущим и
+          maximumValues[i] = max(currentMax, newCompare);
         }
       }
     }
     return maximumValues[W];
   }
 
-  private static int minimumBuy(int W, int n, int[] val, int[] wt) {
+  private static int maximumFromOtherSide(int W, int n, int[] val, int[] wt) {
 
-    int minimumValues[] = new int[W + 1];
+    int maximumValues[] = new int[W + 1];
 
-    for (int i = 0; i <= W; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = W; i >= 0; i--) {
+      for (int j = n - 1; j >= 0; j--) {
         int currentBottleWeight = wt[j];
-        if (currentBottleWeight <= i) { // если вес бутылки меньше текущей итерации
-          int iMinusWeight = i - currentBottleWeight;
+        if (currentBottleWeight + i <= W) {
+          int iMinusWeight = i + currentBottleWeight;
           int currentValue = val[j];
-          int newCompare = minimumValues[iMinusWeight] + currentValue;
-          int currentMax = minimumValues[i];
-          minimumValues[i] = max(currentMax, newCompare); // максимльное значение между текущим и
+          int newCompare = maximumValues[iMinusWeight] + currentValue;
+          int currentMax = maximumValues[i];
+          maximumValues[i] = max(currentMax, newCompare);
         }
       }
     }
-    return minimumValues[W];
+    return maximumValues[0];
   }
 
   // Driver program
@@ -47,6 +46,7 @@ public class UnboundedKnapsack {
     int val[] = {20, 30, 70, 90};
     int wt[] = {1, 2, 4, 8};
     int n = val.length;
-    System.out.println(minimumBuy(W, n, val, wt));
+    System.out.println(maximumBuy(W, n, val, wt));
+    System.out.println(maximumFromOtherSide(W, n, val, wt));
   }
 }

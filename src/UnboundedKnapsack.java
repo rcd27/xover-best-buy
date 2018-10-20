8b9bf514c5f23f063d3ec7daf80aa17e8a6f3399
Zemlyakov Stas <redtom@yandex.ru>
2018-10-20 21:04:25 +0300

    Расписал каждую переменную потому что я тупой

--- src/UnboundedKnapsack.java
+++ src/UnboundedKnapsack.java
@@ -1,36 +1,32 @@
 import static java.lang.Math.max;
 
-// Java program to find maximum achievable
-// value with a knapsack of weight W and 
-// multiple instances allowed. 
 public class UnboundedKnapsack {
 
-  // Returns the maximum value with knapsack
-  // of W capacity
   private static int unboundedKnapsack(int W, int n, int[] val, int[] wt) {
 
-    // dp[i] is going to store maximum value
-    // with knapsack capacity i.
-    int dp[] = new int[W + 1];
+    int maximumValues[] = new int[W + 1];
 
-    // Fill dp[] using above recursive formula
     for (int i = 0; i <= W; i++) {
       for (int j = 0; j < n; j++) {
-        if (wt[j] <= i) {
-          dp[i] = max(dp[i], dp[i - wt[j]] + val[j]);
+        int currentBottleWeight = wt[j];
+        if (currentBottleWeight <= i) { // если вес бутылки меньше текущей итерации
+          int iMinusWeight = i - currentBottleWeight;
+          int currentValue = val[j];
+          int newCompare = maximumValues[iMinusWeight] + currentValue;
+          int currentMax = maximumValues[i];
+          maximumValues[i] = max(currentMax, newCompare); // максимльное значение между текущим и
         }
       }
     }
-    return dp[W];
+    return maximumValues[W];
   }
 
   // Driver program
   public static void main(String[] args) {
     int W = 12;
-    int val[] = {20, 30, 70, 90, 1000, 3000};
-    int wt[] = {1, 2, 4, 8, 5,7};
+    int val[] = {20, 30, 70, 90};
+    int wt[] = {1, 2, 4, 8, 5, 7};
     int n = val.length;
     System.out.println(unboundedKnapsack(W, n, val, wt));
   }
 }
-// This code is contributed by Aditya Kumar  

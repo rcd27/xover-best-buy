d31bff209b4c00bbba99c1de938470a789507e2a
Zemlyakov Stas <redtom@yandex.ru>
2018-10-25 15:20:04 +0300

    Bubble sort all data

--- src/ContinuousKnapsack.java
+++ src/ContinuousKnapsack.java
@@ -9,8 +9,30 @@ public class ContinuousKnapsack {
     for (int i = 0; i < n; i++) {
       double currentP = (double) v[i] / w[i];
       p[i] = currentP;
-
     }
+
+    // Bubble sort
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (p[j] > p[j + 1]) {
+          // sort p[i]
+          double tempP = p[j];
+          p[j] = p[j + 1];
+          p[j + 1] = tempP;
+
+          // sort w[i]
+          long tempW = w[j];
+          w[j] = w[j + 1];
+          w[j + 1] = tempW;
+
+          // sort v[i]
+          long tempV = v[j];
+          v[j] = v[j + 1];
+          v[j + 1] = tempV;
+        }
+      }
+    }
+
     return 100;
   }
 
